cb59c68d063592e37956eba84e3a745d127a1de5
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-08-01 17:32:04 +0800

    map

---
+++ backend/availbikes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AvailbikesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "availbikes"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,12 +40,19 @@ INSTALLED_APPS = [
     "faq",
     "posts",
     "rent",
+    "availbikes",
     "rest_framework",
     "corsheaders", #to allow front and back work together
+    "whitenoise"
 ]
+
+# Configure WhiteNoise to serve static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -126,8 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
